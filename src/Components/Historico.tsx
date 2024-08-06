7f6bd5d3484c1066650cb973868e66a23ecc0fbf
Jose Cardoso <josedeveloperJS@gmail.com>
2024-08-06 10:17:18 -0300

    correção da renderização da lista de saida de itens por funcionário

--- src/Components/Historico.tsx
+++ src/Components/Historico.tsx
@@ -8,49 +8,55 @@ interface PropsSaida {
   data?: Timestamp;
 }
 
-//db
+// db
 import { db } from "../FireBase/FireBase";
 
-//firevase
+// firebase
 import { getDocs, collection, query, Timestamp } from "firebase/firestore";
 
 export function Historico({ nomeSaida }: PropsSaida) {
   const [historico, setHistorico] = useState<PropsSaida[]>([]);
   const [nomeSaidaReg, setNomeSaidaReg] = useState("");
-  console.log(nomeSaidaReg);
+  const [listaDeSaidas, setListaDeSaidas] = useState<PropsSaida[]>([]);
 
   useEffect(() => {
     setNomeSaidaReg(nomeSaida);
   }, [nomeSaida]);
 
   useEffect(() => {
-    async function getItensSaidas() {
-      const linksRef = collection(db, "Saidas");
-      const queryRef = query(linksRef);
+    setHistorico([]); // limpa o estado historico
+    getItensSaidas(); // executa novamente a função getItensSaidas
+  }, [nomeSaidaReg]);
 
-      await getDocs(queryRef).then((snapshot) => {
-        let listadeItens = [] as PropsSaida[];
+  async function getItensSaidas() {
+    const linksRef = collection(db, "Saidas");
+    const queryRef = query(linksRef);
 
-        snapshot.forEach((doc) => {
-          listadeItens.push({
-            id: doc.data().id, //nao achou
-            nomeSaida: doc.data().nome,
-            nomeProd: doc.data().nomeProduto,
-            quantidade: doc.data().quantidadeSaida,
-            data: doc.data().data,
-          });
+    await getDocs(queryRef).then((snapshot) => {
+      let listadeItens = [] as PropsSaida[];
+
+      snapshot.forEach((doc) => {
+        listadeItens.push({
+          id: doc.data().id,
+          nomeSaida: doc.data().nome,
+          nomeProd: doc.data().nomeProduto,
+          quantidade: doc.data().quantidadeSaida,
+          data: doc.data().data,
         });
-
-        setHistorico(listadeItens);
       });
+
+      setHistorico(listadeItens);
+    });
+  }
+
+  useEffect(() => {
+    if (nomeSaidaReg) {
+      const filteredHistorico = historico.filter(
+        (funcionario) => funcionario.nomeSaida === nomeSaidaReg
+      );
+      setListaDeSaidas(filteredHistorico);
     }
-
-    return () => {
-      getItensSaidas();
-    };
-  }, [nomeSaida]);
-
-  console.log(historico);
+  }, [nomeSaidaReg, historico]);
 
   function formatDate(date: Timestamp | undefined) {
     if (date) {
@@ -62,41 +68,32 @@ export function Historico({ nomeSaida }: PropsSaida) {
 
   return (
     <div className="bg-white h-full p-3">
-      {historico.filter((funcionario) => funcionario.nomeSaida === nomeSaida)
-        .length > 0 ? (
-        historico
-          .filter((funcionario) => funcionario.nomeSaida === nomeSaida)
-          .map((item) => (
-            <div
-              className="flex p-1 gap-1 justify-between max-w-full border-solid border-b-2 border-green-400"
-              key={item.id}
-            >
-              <div className="flex gap-1">
-                <p>Nome do item:</p>
-                <p className="text-red-700 font-semibold italic">
-                  {item.nomeProd}
-                </p>
-              </div>
-              <div className="flex gap-1">
-                <p>Data da saída:</p>
-                <p className="text-red-700 font-semibold italic">
-                  {formatDate(item.data)}
-                </p>
-              </div>
-              <div className="flex gap-1">
-                <p>Quantidade:</p>
-                <p className="text-red-700 font-semibold italic">
-                  {item.quantidade}
-                </p>
-              </div>
-              <div>
-                <p>🟢</p>
-              </div>
-            </div>
-          ))
-      ) : (
-        <p>Nenhuma saída encontrada</p>
-      )}
+      {listaDeSaidas?.map((item) => (
+        <div
+          className="flex p-1 gap-1 justify-between max-w-full border-solid border-b-2 border-green-400"
+          key={item.id}
+        >
+          <div className="flex gap-1">
+            <p>Nome do item:</p>
+            <p className="text-red-700 font-semibold italic">{item.nomeProd}</p>
+          </div>
+          <div className="flex gap-1">
+            <p>Data da saída:</p>
+            <p className="text-red-700 font-semibold italic">
+              {formatDate(item.data)}
+            </p>
+          </div>
+          <div className="flex gap-1">
+            <p>Quantidade:</p>
+            <p className="text-red-700 font-semibold italic">
+              {item.quantidade}
+            </p>
+          </div>
+          <div>
+            <p>🟢</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
